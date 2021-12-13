2ccbb5472624901b64c715f98dcb817303a04f39
alexander-griffen <alexandergriffen@protonmail.com>
2021-12-13 20:15:05 +0000

    Day 13

---
+++ src/day13_1.py
@@ -0,0 +1,54 @@
+import utils
+
+sheet = utils.get_input()
+
+coords = []
+folds = []
+for x in sheet:
+    if x != '':
+        if x[0].isalpha():
+            c = x.split(' ')[-1].split('=')
+            folds.append([c[0], int(c[1])])
+        elif x[0].isnumeric():
+            coords.append([int(y) for y in x.split(',')])
+
+max_x = 0
+max_y = 0
+for i in coords:
+    if i[0] > max_x:
+        max_x = i[0]
+    if i[1] > max_y:
+        max_y = i[1]
+
+transparent = [[0 for i in range(max_x + 1)] for j in range(max_y + 1)]
+
+# Fill out the page
+for dot in coords:
+    transparent[dot[1]][dot[0]] = 1
+
+
+# do the folds
+folds = [folds[0]]
+
+for fold in folds:
+    gap = fold[1]
+    if fold[0] == 'y':
+        #fold UP
+        new_transparent = transparent[:gap]
+        old_transparent = transparent[gap:]
+        for y, row in enumerate(old_transparent):
+            for x, point in enumerate(row):
+                if point == 1 and y >= 0:
+                    new_transparent[gap - y][x] = 1
+    else:
+        #fold LEFT
+        new_transparent = [row[:gap] for row in transparent]
+        old_transparent = [row[gap:] for row in transparent]
+        for y, row in enumerate(old_transparent):
+            for x, point in enumerate(row):
+                if point == 1 and x >= 0:
+                    new_transparent[y][gap - x] = 1
+    transparent = new_transparent
+
+print(sum([sum(row) for row in transparent]))
+
